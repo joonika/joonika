94bfb23efc5326531e34c419637022faebec3751
mnjadidi <mnjadidi@gmail.com>
2021-09-15 09:55:28 +0430

    fix Routing speedup

--- src/dev/temp/indexTheme.php
+++ src/dev/temp/indexTheme.php
@@ -1,35 +1,39 @@
 <?php
 $title = 'welcome';
-$dirCheck = file_get_contents(__DIR__ . '/../../../composer.json');
-if ($dirCheck) {
-    $jsonDecode = json_decode($dirCheck, JSON_UNESCAPED_UNICODE);
-    if (!empty($jsonDecode['description'])) {
-        $title = $jsonDecode['description'];
-    } elseif (!empty($jsonDecode['name'])) {
-        $title = $jsonDecode['name'];
+$jsonDecode = \Joonika\helper\Cache::get('themeSampleJson');
+if (empty($jsonDecode)) {
+    $dirCheck = file_get_contents(__DIR__ . '/../../../composer.json');
+    if ($dirCheck) {
+        $jsonDecode = json_decode($dirCheck, JSON_UNESCAPED_UNICODE);
+        \Joonika\helper\Cache::set('themeSampleJson',$jsonDecode);
     }
-} else {
-    echo 'welcome';
 }
-?><html>
+if (!empty($jsonDecode['description'])) {
+    $title = $jsonDecode['description'];
+} elseif (!empty($jsonDecode['name'])) {
+    $title = $jsonDecode['name'];
+}?>
+<html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
     <style>
         .center {
             height: 100px;
             text-align: center;
             position: absolute;
-            top:0;
+            top: 0;
             bottom: 0;
             left: 0;
             right: 0;
             margin: auto;
         }
-        .center .title{
+
+        .center .title {
             font-size: 30px;
         }
-        .center .description{
+
+        .center .description {
             font-size: 25px;
             margin-top: 10px;
             border-top: solid 1px silver;
@@ -40,9 +44,9 @@ if ($dirCheck) {
 <body>
 <div class="center">
     <div>
-        <div class="title"><?=$title?></div>
+        <div class="title"><?= $title ?></div>
         <div class="description">
-            <?=JK_HOST()?>
+            <?= JK_HOST() ?>
         </div>
     </div>
 </div>
