984535248faf86a1cbf233d49342aca212788be8
mnjadidi <mnjadidi@gmail.com>
2021-11-20 20:15:49 +0330

    fix flush cache time in cronjob

--- src/cronjobs/CronJobs.php
+++ src/cronjobs/CronJobs.php
@@ -11,7 +11,7 @@ class CronJobs extends \Joonika\CronJobs
     public function init()
     {
         self::setCronFunction('joonika', 'removeExpiredTemp', '*/5 * * * *', __CLASS__);
-        self::setCronFunction('joonika', 'flushCache', '*/15 * * * *', __CLASS__);
+        self::setCronFunction('joonika', 'flushCache', '2 * * * *', __CLASS__);
     }
 
     public function removeExpiredTemp()
